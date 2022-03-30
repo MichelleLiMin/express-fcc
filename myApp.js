d8b2a5edee3303b1eb804910ca1b6a990be20be6
michelleitaly <michelle22289@gmail.com>
2022-03-30 22:45:53 +0200

    Revert "add json path, nodemon"
    This reverts commit eea2615e01910afa4e6c031d32acd75923523990.

--- myApp.js
+++ myApp.js
@@ -1,13 +1,43 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 console.log("Hello World");
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
+    res.sendFile(__dirname + "/views/index.html");
+  });
+  
+  app.use("/public", express.static(__dirname + "/public"))
 
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
-});
 
-module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
