4c0c7a10c303d71a8ca2db4ecf51ac5abbdea713
michelleitaly <michelle22289@gmail.com>
2022-03-31 19:18:57 +0200

    middleware 3 arguments

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 var express = require("express");
 var app = express();
+app.use((req, res, next)=>{
+  console.log(req.method + " " + req.path + " - " + req.ip )
+  next();
+})
 //process.env.MESSAGE_STYLE = "uppercase";
 //console.log("Hello World");
 app.get("/", (req, res) => {
@@ -8,15 +12,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/json", (req, res) => {
   
 console.log(process.env.MESSAGE_STYLE);
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    //res.json({ message: "Hello json" }).toUpperCase();
+    
     res.json({ message:"Hello json".toUpperCase() });
   } else {
     res.json({ message: "Hello json" });
   }
 });
-
 module.exports = app;
