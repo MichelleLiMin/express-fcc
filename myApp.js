4121fb6ae48e828ce4b7a3dc42985d6b62fb9c16
michelleitaly <michelle22289@gmail.com>
2022-03-31 21:37:30 +0200

    Chain Middleware

--- myApp.js
+++ myApp.js
@@ -1,24 +1,34 @@
-require('dotenv').config()
+require("dotenv").config();
 var express = require("express");
 var app = express();
-app.use((req, res, next)=>{
-  console.log(req.method + " " + req.path + " - " + req.ip )
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+    //console.log(req.time);
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+app.use((req, res, next) => {
+  // console.log(req.method + " " + req.path + " - " + req.ip )
   next();
-})
-//process.env.MESSAGE_STYLE = "uppercase";
+});
+
 //console.log("Hello World");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+  // console.log(__dirname)
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-  
-console.log(process.env.MESSAGE_STYLE);
+  //console.log(process.env.MESSAGE_STYLE);
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    
-    res.json({ message:"Hello json".toUpperCase() });
+    res.json({ message: "Hello json".toUpperCase() });
   } else {
     res.json({ message: "Hello json" });
   }
